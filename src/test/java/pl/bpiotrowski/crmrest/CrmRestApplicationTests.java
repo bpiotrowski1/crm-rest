97d346772cc6786fe815d0d0a79f73327b84ed85
bpiotrowski1 <baej.p26@gmail.com>
2022-03-03 18:55:42 +0100

    Initial commit

---
+++ src/test/java/pl/bpiotrowski/crmrest/CrmRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiotrowski.crmrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
